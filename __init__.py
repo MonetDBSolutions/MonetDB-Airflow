576cfef0dba0dd55738cf1a3f3b3c080746edaf2
MitchellWeg <mitchell.w@live.nl>
2021-12-14 16:07:24 +0100

    add MonetDBHook

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 from airflow.plugins_manager import AirflowPlugin
 
 from monetdb_plugin.operators.monetdb_operator import MonetDBOperator
+from monetdb_plugin.hooks.monetdb_hook import MonetDBHook
 
 class MonetDBPlugin(AirflowPlugin):
     name = "monetdb_plugin"
     operators = [MonetDBOperator]
     sensors = []
-    hooks = []
+    hooks = [MonetDBHook]
     executors = []
     admin_views = []
     flask_blueprints = []
--- operators/monetdb_operator.py
+++ operators/monetdb_operator.py
@@ -1,9 +1,9 @@
 from airflow.models.baseoperator import BaseOperator
 
 class MonetDBOperator(BaseOperator):
-    def __init__(self, name: str, **kwargs) -> None:
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.name = name
 
     def execute(self, context):
+        self.hook = MonetDBHook
         pass
