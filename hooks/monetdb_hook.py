8c212705f68d72c4d4adc978d4128a3b6822fd21
MitchellWeg <mitchell.w@live.nl>
2021-12-15 12:36:17 +0100

    implement hook

--- hooks/monetdb_hook.py
+++ hooks/monetdb_hook.py
@@ -1,4 +1,32 @@
+from typing import Dict
 from airflow.hooks.dbapi import DbApiHook
+from airflow.models import Connection
+
+import pymonetdb
 
 class MonetDBHook(DbApiHook):
-    pass
+
+    conn_name_attr = 'monetdb_conn_id'
+    default_conn_name = 'monetdb_default'
+    conn_type = 'monetdb'
+    hook_name = 'MonetDB'
+    supports_autocommit = True
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.schema = kwargs.pop("schema", None)
+        self.connection = kwargs.pop("connection", None)
+
+    def get_conn(self) -> pymonetdb.sql.connections.Connection:
+        conn = self.connection or self.get_connection(getattr(self, self.conn_name_attr))
+        info = self._get_conn_config(conn)
+        return pymonetdb.connect(**info)
+
+
+    def _get_conn_config(self, conn: Connection) -> Dict:
+        return {
+            'username': conn.login,
+            'password': conn.password,
+            'hostname': conn.host,
+            'database': conn.schema,
+        }
--- operators/monetdb_operator.py
+++ operators/monetdb_operator.py
@@ -1,12 +1,29 @@
 from airflow.models.baseoperator import BaseOperator
 
+from typing import Dict, Iterable, List, Mapping, Optional, Union
+
 from monetdb_plugin.hooks.monetdb_hook import MonetDBHook
 
 
 class MonetDBOperator(BaseOperator):
-    def __init__(self, **kwargs) -> None:
+    def __init__(
+        self,
+        *,
+        sql: Union[str, List[str]],
+        monetdb_conn_id: str = 'monetdb_default',
+        parameters: Optional[Union[Mapping, Iterable]] = None,
+        autocommit: bool = False,
+        database: Optional[str] = None,
+        **kwargs,
+    ) -> None:
         super().__init__(**kwargs)
+        self.monetdb_conn_id = monetdb_conn_id
+        self.sql = sql
+        self.autocommit = autocommit
+        self.parameters = parameters
+        self.database = database
 
     def execute(self, context):
-        self.hook = MonetDBHook
-        pass
+        self.hook = MonetDBHook(monetdb_conn_id=self.monetdb_conn_id, schema=self.database)
+        self.log.info(f"executing {self.sql}")
+        self.hook.run(sql=self.sql)
